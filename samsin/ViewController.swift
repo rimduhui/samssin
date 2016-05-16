fe4f2bb66d8b1774e6389137f9aa75a017af76fc
Kang-Woo Jeong <rkddnnet@naver.com>
2016-05-16 17:54:05 +0900

    Initial Commit

---
+++ samsin/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  samsin
+//
+//  Created by Release on 2016. 5. 16..
+//  Copyright © 2016년 samsin. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+

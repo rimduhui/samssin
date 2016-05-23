981f67bd702dba1c2470e90895b87f348053a4fa
duhui rim <idyyy2002@gmail.com>
2016-05-23 16:34:19 +0900

    samssin1.0

---
+++ samsin/SecondViewController.swift
@@ -0,0 +1,74 @@
+//
+//  SecondViewController.swift
+//  samsin
+//
+//  Created by Release on 2016. 5. 16..
+//  Copyright © 2016년 samsin. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    //var stageBtn[UIButton];
+    var UserColor:CGFloat = 0
+
+    func touchBtn(sender : UIButton) {
+        NSLog("%d가 눌렸습니다.", sender.tag)
+        print("%s", UserColor)
+        sender.backgroundColor = UIColor(red: UserColor, green: UserColor, blue: UserColor, alpha: 1);
+        
+        // server로 보내서 값받아와서 검은색이나 흰색으로 찍으면 된다. http / socket
+        
+        
+        
+        
+        
+    }
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+        self.view.backgroundColor = UIColor(red: 0.94, green: 0.28, blue: 0.26, alpha: 1)
+        
+        var width : CGFloat = self.view.frame.size.width;
+        var height : CGFloat = self.view.frame.size.height;
+
+        
+        for var y : CGFloat = 0 ; y < 15 ; ++y {
+            for var x : CGFloat = 0 ; x < 15 ; ++x {
+                
+                var stageBtn = UIButton();
+                
+                stageBtn.frame.size.width = 24;
+                stageBtn.frame.size.height = 24;
+                stageBtn.frame.origin.x = 20 + 25 * (x % 15);
+                stageBtn.frame.origin.y = 100 + (height/15 + 25*y);
+                stageBtn.backgroundColor = UIColor(red:0.78, green:0.21, blue:0.19, alpha: 1);
+                stageBtn.layer.cornerRadius = 24;
+                
+                // stageBtn.setTitle(NSString(format: "%lu", (NSInteger)(x+1+y*4)), forState:UIControlState.Normal);
+                stageBtn.setTitleColor(UIColor(white: 1, alpha: 1), forState: UIControlState.Normal);
+                stageBtn.tag = (NSInteger)(x+1+y*15);
+                stageBtn.addTarget(self, action: "touchBtn:", forControlEvents: UIControlEvents.TouchDown);
+                
+                self.view.addSubview(stageBtn);
+            }
+        }
+    }
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
--- samsin/ViewController.swift
+++ samsin/ViewController.swift
@@ -8,11 +8,37 @@
 
 import UIKit
 
+
+
 class ViewController: UIViewController {
 
+    @IBOutlet weak var BlackButton: UIButton!
+    
+    @IBOutlet weak var WhiteButton: UIButton!
+    
+    var choiceColor : CGFloat = 0;
+    
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        var UserValue = segue.destinationViewController as! SecondViewController
+        UserValue.UserColor = choiceColor;
+    }
+    
+    func touchBtn1(sender : UIButton) {
+        NSLog("블랙이 눌렸습니다.")
+        choiceColor = 0
+        //sender.backgroundColor = UIColor(red:1, green:1, blue:1, alpha: 1);
+    }
+    func touchBtn2(sender : UIButton) {
+        NSLog("화이트가 눌렸습니다.")
+        choiceColor = 1
+        //sender.backgroundColor = UIColor(red:1, green:1, blue:1, alpha: 1);
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        BlackButton.addTarget(self, action: "touchBtn1:", forControlEvents: UIControlEvents.TouchDown);
+        WhiteButton.addTarget(self, action: "touchBtn2:", forControlEvents: UIControlEvents.TouchDown);
     }
 
     override func didReceiveMemoryWarning() {
@@ -20,6 +46,4 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
-
 }
-
